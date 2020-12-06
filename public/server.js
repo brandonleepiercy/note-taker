0d37395b3fadec556336eafd79ce19865b32f5d7
Brandon Piercy <b.piercy567@gmail.com>
2020-12-06 10:43:03 -0800

    server spins on port 7777

--- public/server.js
+++ public/server.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const app = express();
+const port = 7777;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
