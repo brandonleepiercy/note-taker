005ffd7ecdfe6540d3430801b56f422e63fb94b6
Brandon Piercy <b.piercy567@gmail.com>
2020-12-06 11:25:30 -0800

    added comments

--- public/server.js
+++ public/server.js
@@ -1,13 +1,23 @@
+// Module requirements
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 7777;
 
+// Get method for the default path
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/index.html"));
 });
 
+// Get method for the notes path
+app.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "/notes.html"));
+    res.sendFile(path.join(__dirname, "/assets/css/styles.css"));
+});
+
+
+//Spinning server on the port defined in the module requirements
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
