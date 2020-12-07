d7d94a284c9f05c327cf4a2df7aa0f0f6c76fc21
Brandon Piercy <b.piercy567@gmail.com>
2020-12-06 19:01:05 -0800

    created a post function to the db.json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,22 @@
 //Imports the data from the db.json file
 const db = require("../db/db.json");
+const fs = require('fs');
 
 //Exports a function to server.js file that handles api quests
 module.exports = (app) => {
     app.get("/api/notes", (req,res) => {
         res.json(db);
     });
+
+    app.post("/api/notes", (req,res) => {
+        let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+        let newNote = req.body;
+        let noteID = (savedNotes.length).toString();
+        newNote.id = noteID;
+        savedNotes.push(newNote);
+
+        fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+        console.log("New note saved to the .db file, note: "+newNote);
+        res.json(db);
+    })
 };
