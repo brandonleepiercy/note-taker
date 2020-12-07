925bffe033a5cbb1ce81f4633867ca7e12d61c06
Brandon Piercy <b.piercy567@gmail.com>
2020-12-07 10:01:32 -0800

    delete route now works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,23 +17,25 @@ module.exports = (app) => {
 
         fs.writeFileSync("./db/db.json", JSON.stringify(db));
         console.log("New note saved to the .db file, note: "+newNote);
-        res.json(db);
+        res.json(true);
     });
 
-    app.delete("/api/notes", (req,res) => {
-        let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+    app.delete("/api/notes/:id", (req,res) => {
         let noteID = req.params.id;
         let newID = 0;
         console.log(`Deleting note with ID ${noteID}`);
-        savedNotes = savedNotes.filter((currNote) => {
-            return currNote.id != noteID;
-        })
-        for (currNote of savedNotes) {
+        // const filterForDelete = db.filter((currNote) => {
+        //     return currNote.id != noteID;
+        // })
+
+        db.splice(noteID, 1)
+        for (currNote of db) {
             currNote.id = newID.toString();
             newID++;
-        }
+        };
+        console.log(db)
         
-        fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-        res.json(db);
+        fs.writeFileSync("./db/db.json", JSON.stringify(db));
+        res.json(true);
     });
 };
